5dcdf45bae11d5a4c58c7a48fdd5db8efb4a67bc
thor <thor@localhost.localdomain>
2018-08-03 17:04:07 +0200

    First minimalistic build, whitout proof of possession

--- proofofconcept.ino
+++ proofofconcept.ino
@@ -24,15 +24,13 @@ RestServer rest(server);
 IPAddress ip(192, 168, 192, 80);
 
 void getConnexion(char* query = "", char* body = "", char* bearer = "") {
-
-  uint8_t clientSecret[16] = {'A','B','C','D','E','F','G','H','I','J','0','1','2','3','4','5'};
   
+  uint8_t clientSecret[16] = {'A','B','C','D','E','F','G','H','I','J','0','1','2','3','4','5'};
   // send jwt to Oauth2.0 server
   RestClient client = RestClient("jsonplaceholder.typicode.com");
   String response = "";
   //client.setHeader(bearer);
-  int statusCode = client.post("/posts",bearer, &response);
-  Serial.println(statusCode);
+  int statusCode = client.get("/posts/1", &response);
 
   if (200 == statusCode) {
     // Receive json en parse response
@@ -51,7 +49,6 @@ void getConnexion(char* query = "", char* body = "", char* bearer = "") {
   
     //decrypt key
     aes128_dec_single(clientSecret, decoded);
-    Serial.println(decoded);
   
     //set global key
     strcpy(key,decoded);
@@ -64,17 +61,31 @@ void getConnexion(char* query = "", char* body = "", char* bearer = "") {
 }
 
 void getWeather(char* query = "", char* body = "", char* bearer = "") {
-  rest.addData("key", key);
-  rest.sendResponse(OK,"application/json",0);
-}
+  if (strlen(key) > 0) {
+    //some sort of data to return
+    char data[] = {'B','A','C','H','E','L','O','R','O','A','U','T','H','2','.','0'};
+    
+    //encrpyt
+    aes128_enc_single(key, data);
 
-void doDisconnexion(char* query = "", char* body = "", char* bearer = "") {
-  memset(key, 0, sizeof(key));
-  rest.sendResponse(NO_CONTENT,"application/json",0);
+    //reseting key
+    memset(key, 0, sizeof(key));
+  
+    // base 64 encoding
+    int inputLen = sizeof(data);
+    int encodedLength = base64_enc_len(inputLen);
+    char encoded[encodedLength];
+    base64_encode(encoded, data, encodedLength);
+
+    //attach and send data
+    rest.addData("encoded", encoded);
+    rest.sendResponse(OK,"application/json",0);
+  } else {
+    rest.sendResponse(NO_CONTENT,"application/json",0);
+  }
 }
 
 void notFound(char* data = "") {
-  rest.addData("Not Found", data);
   rest.sendResponse(NOT_FOUND,"application/json",0);
 }
 
@@ -83,7 +94,6 @@ void setup() {
   #if DEBUG
     Serial.begin(9600); //opens serial port, sets data rate to 9600bps
   #endif
-  
   pinMode(LED_BUILTIN, OUTPUT);
 
   Ethernet.begin(mac,ip);
@@ -91,71 +101,9 @@ void setup() {
   server.begin();
   Serial.println(Ethernet.localIP());
 
-  /*
-  //uint8_t iv[16] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
-  
-  //char c[32] = {0x41,0x42,0x43,0x44,0x45 ,0x46,0x47,0x48 ,0x49,0x4a,0x4b,0x4c,0x4d,0x4e,0x4f,0x50,0x51,0x52,0x53,0x54,0x55,0x56,0x57,0x58,0x59,0x5a,0x30,0x31,0x32,0x33,0x34,0x35};
-  //uint8_t* key1 = (uint8_t *)c; // 16 bits keys casted from a char array, win
-  // char + 1 => \O
-  char data[17] = "ABCDEFGHIJ012345"; //16 chars == 16 bytes => char data[17] should be mod 16 => 16 + 1 || 32 + 1 || 48 + 1 etc..
-  
-  uint16_t dataLength = sizeof(data) - 1;
-  //char data[16] = {'0','1','2','3','4','5','6','7','8','9','0','1','2','3','4','5'};
-  //uint16_t eLen = ((sizeof(data) - 1)|15)+1;
-  Serial.print("Plain text : ");
-  Serial.println(data);
-  
-  //aes128_cbc_enc(key1, iv, data, sizeof(data) - 1);
-  //aes256_enc_multiple(key1, data, dataLength); // 256 bit without iv
-  aes128_enc_single(clientSecret, data);
-  // base 64 encoding
-  int inputLen = sizeof(data);
-  int encodedLength = base64_enc_len(inputLen);
-  char encoded[encodedLength];
-  base64_encode(encoded, data, encodedLength); 
-
-  // encrypted char => non-ascii
-  Serial.print("encrypted : ");
-  Serial.println(data);
-  // encoded encrypted => ascii
-  Serial.print("encoded : ");
-  Serial.println(encoded);
-
-  //decode encrypted => non-ascii
-  int input2Len = sizeof(encoded);
-  int decodedLength = base64_dec_len(encoded, input2Len);
-  char decoded[decodedLength];
-  base64_decode(decoded, encoded, input2Len);
-  Serial.print("decoded : ");
-  Serial.println(decoded);
-  //uint16_t dLen = ((sizeof(decoded) - 1)|15)+1; // modulo experession to find the nearest multiple of 16
-  //Serial.println(dLen);
-
-  //char decrypted[data];
-  //strncpy(decrypted, data, (uint16_t)sizeof(data)); // copy encrypted data to decrypted variable
-  
-  uint8_t key2[32] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5'};
-  //aes128_cbc_dec(key2, iv, decoded, sizeof(decoded) - 1);
-  //aes256_dec_multiple(key2, decoded, dataLength); // 256 bit without iv, need to pass a length in request
-  aes128_dec_single(clientSecret, decoded);
-  Serial.print("decrypted : ");
-  Serial.println(decoded);
-  */
-  
-
   rest.addRoute(GET, "/weather", getWeather);
   rest.addRoute(GET, "/connect", getConnexion);
-  //rest.addRoute(DELETE, "/disconnect", doDisconnexion);
   rest.onNotFound(notFound);
-
-  //RestClient client = RestClient("jsonplaceholder.typicode.com");
-  //String response = "";
-  //int statusCode = client.get("/todos/1", &response);
-  //Serial.print("Status code from server: ");
-  //Serial.println(statusCode);
-  //Serial.print("Response body from server: ");
-  //Serial.println(response);
-
 }
 
 void loop() {
